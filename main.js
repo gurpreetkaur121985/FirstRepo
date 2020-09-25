b6af682b79721a0e2bfaaab62a2967a535df3dd0
gurpreetkaur121985 <71897312+gurpreetkaur121985@users.noreply.github.com>
2020-09-25 22:54:24 +0530

    Add files via upload
    First Commit

---
+++ main.js
@@ -0,0 +1,3 @@
+function testone(){
+  window.alert(The Great Wall of China  is the collective name of a series of fortification systems generally built across the historical northern borders of China to protect and consolidate territories of Chinese states and empires against various nomadic groups of the steppe and their polities);
+}
